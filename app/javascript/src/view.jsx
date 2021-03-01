7bb4c2ef47aa43e82ac353d3509d61bb5c9806f1
Norelle M <NorelleMan@Norelle-Mans-MacBook-Pro.local>
2021-03-02 01:23:34 +0800

    Emarsys Web Extend

--- app/javascript/src/view.jsx
+++ app/javascript/src/view.jsx
@@ -3,16 +3,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './layout';
 
-const Demo = () => (
-  <Layout>
-    <div className="container">
-      <h1>Products</h1>
-      <h4>Tshirt</h4>
-      <h5>$100</h5>
-      <img src="https://drive.google.com/uc?id=1Jh3zJQKnAAFpEkubFVpU3OUhVTLPghng" alt="tshirt" width="200" />
-    </div>
-  </Layout>
-)
+class Demo extends React.Component{
+  state = {
+    id: 'ems-demo-hk-1',
+    title: 'Tshirt',
+    price: 100,
+    image: 'https://drive.google.com/uc?id=1Jh3zJQKnAAFpEkubFVpU3OUhVTLPghng'
+  }
+
+  WebExtendView = (id) => {
+    ScarabQueue.push(['view', id]);
+    ScarabQueue.push(['go']);
+  }
+
+  render () {
+    const { id, title, price, image } = this.state;
+    return (
+      <Layout>
+        <div className="container">
+          <h1>Products</h1>
+          <h4>{title}</h4>
+          <h5>${price}</h5>
+          <img src={image} alt={title} width="200" />
+          <div>
+            <button className="btn btn-dark" onClick={this.WebExtendView(id)}>Send View</button>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
