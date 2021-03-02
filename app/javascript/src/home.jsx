c644de93596c959d6ad49bec4f4db37fcecad193
Norelle M <NorelleMan@Norelle-Mans-MacBook-Pro.local>
2021-03-02 15:18:22 +0800

    internal ad 1

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -5,7 +5,13 @@ import Layout from './layout';
 
 const Home = () => (
   <Layout>
-    <h1>Home page</h1>
+    <div className="container">
+      <h1>Home page</h1>
+      <h4>Internal ad 1</h4>
+      <a href="/view">
+        <img src="https://doc-0k-9o-docs.googleusercontent.com/docs/securesc/q12n5n9dh0glpu3l2mee1arv2nd5tc64/tmsdp41t294n9vfkt11i15q2il18q31o/1614664650000/08929955126707880999/03277970015415550772/1uhpdpC2ugvJsMo-iszMse4pGmgn0BZBg?authuser=0" alt="internal-ad-1" width="200" />
+      </a>
+    </div>
   </Layout>
 )
 
