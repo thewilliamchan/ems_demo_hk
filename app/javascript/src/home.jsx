21bfdb7d22ec10184e8fead9b30ff69247fb6829
Norelle M <NorelleMan@Norelle-Mans-MacBook-Pro.local>
2021-03-02 15:48:40 +0800

    added GA ITM url params

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -8,7 +8,7 @@ const Home = () => (
     <div className="container">
       <h1>Home page</h1>
       <h4>Internal ad 1</h4>
-      <a href="/view">
+      <a href="/view?itm_source_h=emsdemohk&itm_medium_h=banner_ad&itm_campaign_h=new_products_2021&itm_content_h=small_banner&itm_term_h=new_products">
         <img src="https://doc-0k-9o-docs.googleusercontent.com/docs/securesc/q12n5n9dh0glpu3l2mee1arv2nd5tc64/tmsdp41t294n9vfkt11i15q2il18q31o/1614664650000/08929955126707880999/03277970015415550772/1uhpdpC2ugvJsMo-iszMse4pGmgn0BZBg?authuser=0" alt="internal-ad-1" width="200" />
       </a>
     </div>
