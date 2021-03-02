cba7d3cc3fe9204843cdd7b58c68fd3b0e21d6ed
Norelle M <NorelleMan@Norelle-Mans-MacBook-Pro.local>
2021-03-02 17:54:49 +0800

    added ad B and updated GA ITM code

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -4,9 +4,15 @@ import ReactDOM from 'react-dom';
 import Layout from './layout';
 
 class Home extends React.Component {
-  SendITM() {
-    // send ITM values to Google Analytics
-    gtag('event', 'view_item', {'itm_source_h': 'emsdemohk', 'itm_medium_h': 'banner', 'itm_campaign_h': 'new-products-2021', 'itm_content_h': 'small-banner', 'itm_term_h': 'new-products'});
+  SendITM_A() {
+    // send ITM values to Google Analytics for ad A
+    gtag('event', 'view_item', {'itm_source_h': 'emsdemohk', 'itm_medium_h': 'banner', 'itm_campaign_h': 'new-products-2021-A', 'itm_content_h': 'small-banner', 'itm_term_h': 'new-products'});
+    console.log("itm data sent");
+  }
+
+  SendITM_B() {
+    // send ITM values to Google Analytics for ad B
+    gtag('event', 'view_item', {'itm_source_h': 'emsdemohk', 'itm_medium_h': 'banner', 'itm_campaign_h': 'new-products-2021-B', 'itm_content_h': 'small-banner', 'itm_term_h': 'new-products'});
     console.log("itm data sent");
   }
 
@@ -15,9 +21,15 @@ class Home extends React.Component {
       <Layout>
         <div className="container">
           <h1>Home page</h1>
-          <h4>Internal ad 1</h4>
-          <a onClick={() => this.SendITM()} href="/view">
-            <img src="https://drive.google.com/uc?id=1uhpdpC2ugvJsMo-iszMse4pGmgn0BZBg" alt="internal-ad-1" width="200" />
+          <h4>Internal ad A</h4>
+          <a onClick={() => this.SendITM_A()} href="/view">
+            <img src="https://drive.google.com/uc?id=1uhpdpC2ugvJsMo-iszMse4pGmgn0BZBg" alt="internal-ad-a" width="200" />
+          </a>
+          <br />
+          <br />
+          <h4>Internal ad B</h4>
+          <a onClick={() => this.SendITM_B()} href="/view">
+            <img src="https://drive.google.com/uc?id=12gO5oML-4UaJ5wDuWUEbSqT8_aN5IXsZ" alt="internal-ad-b" width="200" />
           </a>
         </div>
       </Layout>
